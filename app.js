4170a17e3478e26c646fe58fa49247f7c9b8c7e8
Agustin Romero Rivero <agusrr1305@gmail.com>
2024-05-31 18:04:41 +0200

    app use

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const port = process.env.PORT || 3000;
 
-
+app.use(express.static("public"));
 app.get("/index1.html", (req, res) => {
     res.status(200).sendFile(__dirname + "/public/index1.html");
   });
